a2bf0c784d01124f75e96094debe6a87b6254692
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-07-18 16:44:17 +0900

    created useStorage to set progress, url and err

---
+++ src/Hooks/useStorage.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import { projectStorage } from '../firebase/config'
+
+export const useStorage = (file) => {
+    const [progress, setProgress] = useState(0)
+    const [error, setError] = useState('')
+    //Image url that we get back from storage after a file is fully uploaded
+    const [url, setUrl] = useState('')
+
+    //useEffect(()=> {parameter}, [dependency])
+    //a function inside useEffect is gonna fire 
+    //everytime [dependency] which is file changes 
+    useEffect(()=>{
+        // references
+        const storageRef = projectStorage.ref(file.name)
+
+        //this will take a file and put it in the reference
+        storageRef.put(file).on('state_changed',(snap)=>{
+            //snapshot wile uploading
+            let percentage = (snap.byteTransferred / snap.totalBytes) * 100
+            setProgress(percentage)
+        }, (err) => {
+            setError(err)
+        }, async () => {
+            const url = await storageRef.getDownloadURL()
+            setUrl(url)
+        })
+    },[file])
+
+    return { progress, url, error }
+
+}
+
